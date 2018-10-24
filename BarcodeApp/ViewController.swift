8a71848bc0319b6db61b31503a132cee2e681fff
VPavelDm <pavel.vaitsikhouski@itechart-group.com>
2018-10-24 14:10:02 +0300

    Initial Commit

---
+++ BarcodeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BarcodeApp
+//
+//  Created by mac-089-71 on 10/24/18.
+//  Copyright © 2018 VPavelDm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
