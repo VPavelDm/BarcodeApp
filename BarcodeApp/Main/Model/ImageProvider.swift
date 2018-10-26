e23ef759a1b97a84c6e4ebcb26bc6c5a60216a54
VPavelDm <pavel.vaitsikhouski@itechart-group.com>
2018-10-26 13:58:00 +0300

    Added reading urls from file

--- BarcodeApp/Main/Model/ImageProvider.swift
+++ BarcodeApp/Main/Model/ImageProvider.swift
@@ -12,19 +12,18 @@ import RxSwift
 class ImageProvider {
     
     func getData() -> Observable<[Image]> {
-        return Observable.create { [weak self] observer in
+        return Observable.create { observer in
             defer { observer.onCompleted() }
-            if let images = self?.images {
+            do {
+                let filePath = Bundle.main.url(forResource: "Urls", withExtension: "txt")!
+                let fileContent = try String(contentsOf: filePath)
+                let images = fileContent.split(separator: "\n").map{ URL(string: $0.str)! }.map { Image(url: $0, state: .notLoaded) }
                 observer.onNext(images)
-            }            
+            } catch let error {
+                print(error)
+            }
             return Disposables.create()
         }
     }
-    
-    private let images: [Image] = [
-        Image(url: URL(string: "https://nekusaka.com/wp-content/uploads/2017/02/Shiba-inu11.jpg")!, state: .loaded),
-        Image(url: URL(string: "https://nekusaka.com/wp-content/uploads/2017/02/Shiba-inu11.jpg")!, state: .notLoaded),
-        Image(url: URL(string: "https://nekusaka.com/wp-content/uploads/2017/02/Shiba-inu11.jpg")!, state: .processed)
-    ]
     
 }
